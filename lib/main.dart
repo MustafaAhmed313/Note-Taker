08dff391d52dcd4475ef5b60037c67dfbd8714bb
Mustafa Ahmed <mostafa3132004@gmail.com>
2024-08-18 13:49:00 +0300

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+
+      ),
+    );
+  }
+}
+
